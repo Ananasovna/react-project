07ba073a19d000df00be43b7facfa6a7f3ea0802
Ananasovna <a.n.ushkova@yandex.ru>
2022-11-01 18:07:42 +0200

    refactor: implement friendCard adding with array method

--- src/components/Sidebar/Friends/Friends.jsx
+++ src/components/Sidebar/Friends/Friends.jsx
@@ -2,13 +2,16 @@ import { FriendCard } from '../FriendCard/FriendCard';
 import classes from './Friends.module.css';
 
 export const Friends = (props) => {
+  let friendsArr = props.state.slice(0, 3);
+
   return(
     <div className={classes.friends}>
         <h2 className={classes.h2}>My friends</h2>
         <div className={classes.cardsWrapper}>
-        <FriendCard state={props.state[1]}/>
-        <FriendCard state={props.state[2]}/>
-        <FriendCard state={props.state[3]}/>
+          {friendsArr.map( el => <FriendCard state={el}/>)}
+        {/* // <FriendCard state={props.state[1]}/>
+        // <FriendCard state={props.state[2]}/>
+        // <FriendCard state={props.state[3]}/> */}
         </div>
 
     </div>
