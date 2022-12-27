1117e6ddabed3f7e8cd090f99dfc0ca6ad71921a
Ananasovna <a.n.ushkova@yandex.ru>
2022-12-27 11:51:33 +0200

    feat: add useEffect hook into ProfileStatus component

--- src/components/Profile/UserInfo/ProfileStatus.jsx
+++ src/components/Profile/UserInfo/ProfileStatus.jsx
@@ -1,68 +1,47 @@
-import React, { useState } from "react";
-import styles from '../UserInfo/ProfileStatus.module.css';
+import React, { useState, useEffect } from "react";
+import styles from "../UserInfo/ProfileStatus.module.css";
 
 export const ProfileStatus = (props) => {
-  // state = {
-  //   editMode: false,
-  //   status: this.props.status,
-  // }
+  let [editMode, setEditMode] = useState(false);
+  let [status, setStatus] = useState(props.status);
 
-  // activateEditMode = () => {
-  //   this.setState({editMode: true})
-  // }
+  const activateEditMode = () => {
+    setEditMode(true);
+  };
 
-  // deactivateEditMode = () => {
-  //   this.setState({editMode: false});
-  //   this.props.updateUserStatus(this.state.status);
-  // }
+  const deactivateEditMode = () => {
+    setEditMode(false);
+    props.updateUserStatus(status);
+  };
 
-  // onStatusChange = (event) => {
-  //   this.setState({
-  //     status: event.currentTarget.value
-  //   })
-  // }
+  const onStatusChange = (event) => {
+    setStatus(event.currentTarget.value);
+  };
 
-  // componentDidUpdate = (prevProps, prevState) => {
-  //   if (this.props.status !== prevState.status) {
-  //     this.setState({
-  //       status: this.props.status
-  //     })
-  //   }
-  // }
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
-    let [editMode, setEditMode] = useState(false);
-    let [status, setStatus] = useState(props.status);
-
-    const activateEditMode = () => {
-      setEditMode(true);
-    }
-
-    const deactivateEditMode = () => {
-      setEditMode(false);
-      props.updateUserStatus(status);
-    }
-
-    const onStatusChange = (event) => {
-      setStatus(event.currentTarget.value)
-      }
-
-
-    return (
-      <div>
-        {!editMode ?
+  return (
+    <div>
+      {!editMode ? (
         <div>
-          <span onDoubleClick={activateEditMode}>{props.status || 'No status'}</span>
+          <span onDoubleClick={activateEditMode}>
+            {props.status || "No status"}
+          </span>
         </div>
-        :
+      ) : (
         <div>
-          <input 
-          onChange={onStatusChange} 
-          autoFocus={true} 
-          onBlur={deactivateEditMode} 
-          className={styles.input}
-          value={status} type="text" />
-        </div>}
-      </div>
-    );
-}
-
+          <input
+            onChange={onStatusChange}
+            autoFocus={true}
+            onBlur={deactivateEditMode}
+            className={styles.input}
+            value={status}
+            type="text"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
